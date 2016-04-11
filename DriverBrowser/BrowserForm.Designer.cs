410127ebd8a6644e268e759cf765a875439e3957
Daniel Koics <koics.dani@gmail.com>
2016-04-11 18:27:45 +0200

    Do design (Exercise 1) in Solution 'DriverBrowser'

--- DriverBrowser/BrowserForm.Designer.cs
+++ DriverBrowser/BrowserForm.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.driveBox = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.driveLabel = new System.Windows.Forms.Label();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pathBox
+            // 
+            this.pathBox.Location = new System.Drawing.Point(236, 58);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(316, 20);
+            this.pathBox.TabIndex = 0;
+            // 
+            // driveBox
+            // 
+            this.driveBox.FormattingEnabled = true;
+            this.driveBox.Location = new System.Drawing.Point(236, 21);
+            this.driveBox.Name = "driveBox";
+            this.driveBox.Size = new System.Drawing.Size(316, 21);
+            this.driveBox.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 101);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(540, 173);
+            this.listBox1.TabIndex = 2;
+            // 
+            // openButton
+            // 
+            this.openButton.Enabled = false;
+            this.openButton.Location = new System.Drawing.Point(12, 292);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(88, 23);
+            this.openButton.TabIndex = 3;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Enabled = false;
+            this.deleteButton.Location = new System.Drawing.Point(121, 292);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(89, 23);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // driveLabel
+            // 
+            this.driveLabel.AutoSize = true;
+            this.driveLabel.Location = new System.Drawing.Point(186, 24);
+            this.driveLabel.Name = "driveLabel";
+            this.driveLabel.Size = new System.Drawing.Size(35, 13);
+            this.driveLabel.TabIndex = 5;
+            this.driveLabel.Text = "Drive:";
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(186, 61);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(32, 13);
+            this.pathLabel.TabIndex = 6;
+            this.pathLabel.Text = "Path:";
+            // 
+            // BrowserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(564, 332);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.driveLabel);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.driveBox);
+            this.Controls.Add(this.pathBox);
+            this.Name = "BrowserForm";
+            this.Text = "Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox pathBox;
+        private System.Windows.Forms.ComboBox driveBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Label driveLabel;
+        private System.Windows.Forms.Label pathLabel;
     }
 }
 
