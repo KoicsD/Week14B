891332c6d40efc3fad5f9ee3d250f721a0b8538e
Daniel Koics <koics.dani@gmail.com>
2016-04-11 20:10:57 +0200

    Implement openButton functionality (Exercise 3) in Solution 'DriverScanner'

--- DriverBrowser/BrowserForm.cs
+++ DriverBrowser/BrowserForm.cs
@@ -33,6 +33,27 @@ namespace DriverBrowser
             driveBox.DropDownStyle = ComboBoxStyle.DropDownList;
         }
 
+        private void refreshListBox()
+        {
+            pathBox.Text = selectedDirectory.FullName;
+            ArrayList items = new ArrayList(selectedDirectory.GetDirectories());
+            items.AddRange(selectedDirectory.GetFiles());
+            listBox.DataSource = items.ToArray();
+            if (items.Count > 0)
+            {
+                if (listBox.SelectedItem is DirectoryInfo)
+                    openButton.Enabled = true;
+                else
+                    openButton.Enabled = false;
+                deleteButton.Enabled = true;
+            }
+            else
+            {
+                openButton.Enabled = false;
+                deleteButton.Enabled = false;
+            }
+        }
+
         private void driveBox_SelectedIndexChanged(object sender, System.EventArgs e)
         {
             DriveInfo selectedDrive = (DriveInfo)driveBox.SelectedItem;
@@ -43,11 +64,14 @@ namespace DriverBrowser
                 return;
             }
             selectedDirectory = selectedDrive.RootDirectory;
-            pathBox.Text = selectedDirectory.FullName;
-            ArrayList items = new ArrayList(selectedDirectory.GetDirectories());
-            items.AddRange(selectedDirectory.GetFiles());
-            listBox.DataSource = items.ToArray();
+            refreshListBox();
             selectedIndex = driveBox.SelectedIndex;
+        }
+
+        private void openButton_Click(object sender, System.EventArgs e)
+        {
+            selectedDirectory = (DirectoryInfo)listBox.SelectedItem;
+            refreshListBox();
         }
     }
 }
