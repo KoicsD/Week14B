8b83cd45a452236692842e8d3f26b93a393e5b9b
Daniel Koics <koics.dani@gmail.com>
2016-04-11 16:18:34 +0200

    Do Exercise 2 in Solution 'Collections'

--- Collections/ReverseIntLister/Program.cs
+++ Collections/ReverseIntLister/Program.cs
@@ -1,15 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
-namespace ReverseIntLister
+namespace ReverseIntLister  // Exercise 2
 {
     class Program
     {
+        static Stack integers;
+
+        static bool TryParseIntArgs(string[] args, out Stack ints)
+        {
+            ints = new Stack(args.Length);
+            foreach (string arg in args)
+            {
+                int n;
+                if (int.TryParse(arg, out n))
+                    ints.Push(n);
+                else
+                    return false;
+            }
+            return true;
+        }
+
+        static int AskForPosInt(string prompt)
+        {
+            int value;
+            do
+            {
+                Console.Write(prompt + " ");
+            } while (!(int.TryParse(Console.ReadLine(), out value) && value > 0));
+            return value;
+        }
+
+        static Stack AskForInts(int depth)
+        {
+            Stack ints = new Stack(depth);
+            for (int i = 0; i < depth; ++i)
+            {
+                Console.Write("{0}th integer: ", i + 1);
+                int n;
+                if (int.TryParse(Console.ReadLine(), out n))
+                    ints.Push(n);
+                else
+                    --i;
+            }
+            return ints;
+        }
+
+        static void ConsumeAndPrint(Stack ints)
+        {
+            while (ints.Count > 0)
+                Console.WriteLine(ints.Pop());
+        }
+
         static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                if (!TryParseIntArgs(args, out integers))
+                {
+                    Console.WriteLine("Problem when parsing integers in command-line argument.");
+                    return;
+                }
+            }
+            else
+            {
+                int length = AskForPosInt("How many integers would you like to add?");
+                integers = AskForInts(length);
+            }
+            Console.WriteLine("Your integers in reverse order:");
+            ConsumeAndPrint(integers);
         }
     }
 }
